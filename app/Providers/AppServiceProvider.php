e7d7be2985d4ca16cc3c5c03350540f0a934f7ce
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-04-24 14:36:38 +0300

    feat: add configure using for select and action

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Forms;
+use Filament\Tables;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,50 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Forms\Components\Select::configureUsing(function ($component): void {
+            if (method_exists($component, 'native')) {
+                $component->native(false);
+            }
+        });
+
+        Tables\Filters\SelectFilter::configureUsing(function ($filter): void {
+            if (method_exists($filter, 'native')) {
+                $filter->native(false);
+            }
+        });
+
+        Forms\Components\Component::configureUsing(function ($component): void {
+            if (method_exists($component, 'native')) {
+                $component->native(false);
+            }
+        });
+
+        Tables\Actions\ViewAction::configureUsing(function (Tables\Actions\ViewAction $action): void {
+            $action
+                ->button()
+                ->hiddenLabel()
+                ->tooltip('View');
+        });
+
+        Tables\Actions\EditAction::configureUsing(function (Tables\Actions\EditAction $action): void {
+            $action
+                ->button()
+                ->hiddenLabel()
+                ->tooltip('Edit');
+        });
+
+        Tables\Actions\DeleteAction::configureUsing(function (Tables\Actions\DeleteAction $action): void {
+            $action
+                ->button()
+                ->hiddenLabel()
+                ->tooltip('Delete');
+        });
+
+        Tables\Actions\ReplicateAction::configureUsing(function (Tables\Actions\ReplicateAction $action): void {
+            $action
+                ->button()
+                ->hiddenLabel()
+                ->tooltip('Replicate');
+        });
     }
 }
